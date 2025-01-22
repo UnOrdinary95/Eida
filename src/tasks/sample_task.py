1f64a50e9e6ef5e5225ee0c18fc929bafe9bcdeb
UnOrdinary <114882529+UnOrdinary95@users.noreply.github.com>
2025-01-22 20:26:42 +0100

    comparison between two datetime object d1

--- src/tasks/sample_task.py
+++ src/tasks/sample_task.py
@@ -1,11 +1,25 @@
 import time
+from datetime import datetime
 from discord.ext import tasks
 from src.client_ import client
-from src.tasks.test import unordinary, message1, message2
+from src.tasks.test import unordinary, message1, message2, list_str_time, time_format
+
 
 @tasks.loop(seconds=0.5)
 async def sendpm_custom():
-    current_time = time.localtime()
+    current_time = datetime.now()
+
+    if (list_str_time[0] > current_time):
+        user = await client.fetch_user(unordinary)
+        await user.send(message1)
+        print(datetime.strftime(current_time, time_format), " | MORNING MESSAGE")
+        pass
+
+
+
+
+
+
     match time.strftime("%H:%M:%S", current_time):
         case "04:30:00":
             user = await client.fetch_user(unordinary)
@@ -17,3 +31,5 @@ async def sendpm_custom():
             print(time.strftime("%H:%M:%S", current_time), " | EVENING MESSAGE")
         case _:
             print(time.strftime("%H:%M:%S", current_time))
+
+
