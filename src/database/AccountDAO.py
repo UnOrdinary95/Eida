6ffc448a76a0f075b597af55529afa7e7cb52c21
UnOrdinary <114882529+UnOrdinary95@users.noreply.github.com>
2025-03-14 21:31:58 +0100

    INSERT / UPDATE

--- src/database/AccountDAO.py
+++ src/database/AccountDAO.py
@@ -1,31 +1,55 @@
 import psycopg2
 
+from typing import Optional, Tuple
 from src.database.PostgreSQLDB import PostgreSQLDB
+from src.models.Account import Account
+
+class AccountDAO:
+    @staticmethod
+    def addAccount(anID: int) -> Optional[bool]:
+        with PostgreSQLDB() as db:
+            cursor = db.cursor()
+
+            try:
+                cursor.execute("INSERT INTO account (user_id) VALUES (%s)", (anID,))
+                db.commit()
+                return True
+            except psycopg2.Error as e:
+                db.rollback()
+                print(f"Error inserting user: {e}")
+            finally:
+                cursor.close()
 
 
-# INSERT USER
-with PostgreSQLDB() as db:
-    cursor = db.cursor()
+    @staticmethod
+    def setTimezone(anID: int, aTimezone: str) -> Optional[bool]:
+        if Account.is_a_timezone(aTimezone):
+            with PostgreSQLDB() as db:
+                cursor = db.cursor()
 
-    try:
-        cursor.execute("INSERT INTO account (user_id) VALUES (100)")
-        db.commit()
-    except psycopg2.Error as e:
-        db.rollback()
-        print(f"Error inserting user: {e}")
-    finally:
-        cursor.close()
+                try:
+                    cursor.execute("UPDATE account SET timezone = %s WHERE user_id = %s", (aTimezone, anID))
+                    db.commit()
+                    return True
+                except psycopg2.Error as e:
+                    db.rollback()
+                    print(f"Error updating user timezone: {e}")
+                finally:
+                    cursor.close()
+        else:
+            print("Error updating user timezone: timezone not supported")
+            return False
 
+    @staticmethod
+    def getAccountByID(anID: int) -> Optional[Tuple]:
+        with PostgreSQLDB() as db:
+            with db.cursor() as cursor:
+                try:
+                    cursor.execute("SELECT * FROM account WHERE user_id = %s", (anID,))
+                    return cursor.fetchone()
+                except psycopg2.Error as e:
+                    print(f"Error getting account: {e}")
+                    return None
 
-# UPDATE USER TZ
-with PostgreSQLDB() as db:
-    cursor = db.cursor()
-
-    try:
-        cursor.execute("UPDATE account SET timezone = 'un timezone random' WHERE user_id = 100")
-        db.commit()
-    except psycopg2.Error as e:
-        db.rollback()
-        print(f"Error updating user timezone: {e}")
-    finally:
-        cursor.close()
+# AccountDAO.addAccount(1000)
+# AccountDAO.setTimezone(1000, "Europe/Paris")
