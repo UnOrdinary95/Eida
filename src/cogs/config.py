90123f25b223c8a10c7e60102bbcba9a711061ef
UnOrdinary <114882529+UnOrdinary95@users.noreply.github.com>
2025-08-09 16:59:13 +0200

    Implement /c command -completed

--- src/cogs/config.py
+++ src/cogs/config.py
@@ -1,10 +1,34 @@
+import logging
 import discord
 from discord import app_commands
 from discord.ext import commands
+from src.database.AccountDAO import AccountDAO
+
+logger = logging.getLogger(__name__)
+
 
 class Config(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.embeds_create_account = {
+            "success": discord.Embed(title="✅ Account Creation", description="Account created successfully!"),
+            "error": discord.Embed(title="❌ Account Creation", description="You already have an account or an error occurred."),
+            "warning": discord.Embed(title="⚠️ Account Creation", description="Unexpected error occurred. Please try again later.")
+        }
+        
+    @app_commands.command(name="c", description="Create your personal reminder account.")
+    async def create_account(self, interaction: discord.Interaction):
+        try:
+            success = AccountDAO.addAccount(interaction.user.id)
+            if success:
+                await interaction.response.send_message(embed=self.embeds_create_account["success"], ephemeral=True)
+            else:
+                await interaction.response.send_message(embed=self.embeds_create_account["error"], ephemeral=True)
+        except Exception as e:
+            await interaction.response.send_message(embed=self.embeds_create_account["warning"], ephemeral=True)
+            logger.error(f"Unexpected error occurred while creating account: {e}")
+            
+    # @app_commands.command(name="setz", description="Set your timezone for accurate reminders.") 
 
 async def setup(bot):
     await bot.add_cog(Config(bot))
--- src/database/AccountDAO.py
+++ src/database/AccountDAO.py
@@ -3,6 +3,7 @@ import logging
 from typing import Optional, Tuple
 from src.database.PostgreSQLDB import PostgreSQLDB
 from src.models.Account import Account
+from psycopg.errors import UniqueViolation
 
 logger = logging.getLogger(__name__)
 
@@ -13,10 +14,18 @@ class AccountDAO:
         try:
             with PostgreSQLDB() as connection:
                 with connection.cursor() as cursor:
+                    try:
                         cursor.execute("INSERT INTO account (user_id) VALUES (%s)", (discord_uid,)) # (discord_uid,) => tuple
-                        logger.info(f"New account successfully created => user_id={discord_uid}.") 
                         return True
+                    except UniqueViolation as e: # If the account is already registered
+                        connection.rollback()
+                        logger.warning(f"Account already exists for user_id={discord_uid}.")
+                        return False
+                connection.commit()
+                logger.info(f"New account successfully created => user_id={discord_uid}.")
+                return True
         except psycopg.DatabaseError as e:
+            connection.rollback()
             logger.error(f"Error while inserting new user ({discord_uid}): {e}")
             return False
 
@@ -30,24 +39,27 @@ class AccountDAO:
             with PostgreSQLDB() as connection:
                 with connection.cursor() as cursor:
                     cursor.execute("UPDATE account SET timezone = %s WHERE user_id = %s", (timezone, discord_uid))
-                    logger.info(f"User timezone updated for user_id={discord_uid}")
-                    return True
+                connection.commit()
+                logger.info(f"User timezone updated for user_id={discord_uid}")
+                return True
         except psycopg.DatabaseError as e:
+            connection.rollback()
             logger.error(f"Error updating user_id={discord_uid} timezone: {e}")
             return False
 
     @staticmethod
     def getAccountById(discord_uid: int) -> Optional[Tuple]:
         try:
+            result = None
             with PostgreSQLDB() as connection:
                 with connection.cursor() as cursor:
                     cursor.execute("SELECT * FROM account WHERE user_id = %s", (discord_uid,))
                     result = cursor.fetchone()
-                    if result:
-                        logger.info(f"User info retrieved for user_id={discord_uid}")
-                    else:
-                        logger.info(f"No account found for user_id={discord_uid}")
-                    return result
+                if result:
+                    logger.info(f"User info retrieved for user_id={discord_uid}")
+                else:
+                    logger.info(f"No account found for user_id={discord_uid}")
+                return result
         except psycopg.DatabaseError as e:
             logger.error(f"Error getting account for user_id={discord_uid}: {e}")
-            return None
+            return result
